8a5cefacf56c5b4f2d5dd604cc5e741fa34e54bc
非洲北极熊 <602173141@qq.com>
2022-05-27 15:00:48 +0800

    feat: 修改工程目录

---
+++ src/views/home/index.vue
@@ -0,0 +1,17 @@
+<!-- 
+    @description home页面
+    @author zhangcj
+    @date 2022-05-27 14:56:10 
+ -->
+<template>
+    <div class="home-container"></div>
+</template>
+<script>
+export default {
+    name:'Home',
+    components:{},
+    methods: {},
+}
+</script>
+<style lang="scss" scoped>
+</style>
