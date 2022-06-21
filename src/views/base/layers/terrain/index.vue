9f3c94ca67ef3ec07e0d61a2800a287e3086c550
张超嘉 <602173141@qq.com>
2022-06-21 14:55:50 +0800

    continue: 修改地形示例

--- src/views/base/layers/terrain/index.vue
+++ src/views/base/layers/terrain/index.vue
@@ -7,14 +7,46 @@
   <div class="viewer-container" ref="viewerDOM"></div>
 </template>
 <script>
-import {Viewer} from 'cesium'
+import { Viewer, createWorldTerrain } from 'cesium'
+import layerUtil from '@/utils/cesium/imageryLayerUtil'
 let viewer
 export default {
   mounted() {
     const viewerDOM = this.$refs.viewerDOM
-    viewer = new Viewer(viewerDOM, {
-      infoBox: false,
+    const worldTerrain = createWorldTerrain({
+      // 水面纹理
+      requestWaterMask: true,
+      // 山体阴影
+      requestVertexNormals: true,
     })
+    viewer = new Viewer(viewerDOM, {
+      // 地理位置查询定位控件
+      geocoder: false,
+      // 是否显示home按钮
+      homeButton: false,
+      // 是否显示3D/2D选择器
+      sceneModePicker: false,
+      // 是否显示底图选择器
+      baseLayerPicker: false,
+      // 是否显示右上角的帮助按钮
+      navigationHelpButton: false,
+      // 是否创建动画小部件
+      animation: false,
+      // 是否显示时间轴
+      timeline: false,
+      // 是否显示全屏按钮
+      fullscreenButton: false,
+      //是否显示信息框
+      infoBox: false,
+      // 加载地形
+      terrainProvider: worldTerrain,
+      // 不自动添加影像图层
+      imageryProvider: false,
+    })
+    // 去掉cesium标识
+    viewer.cesiumWidget.creditContainer.style.display = 'none'
+    // 加载天地图
+    layerUtil.addTDTLayer(viewer, 'img', 'img')
   },
   methods: {},
 }
